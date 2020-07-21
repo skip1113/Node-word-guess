a9b14d192c7259da08ffeaec45add5acbd631ee0
Skip Khamvongsa <skipk95@gmail.com>
2020-07-20 21:32:40 -0600

    Lint word.js and corrected unary operators

--- word.js
+++ word.js
@@ -1,32 +1,31 @@
-//Requiring exported file/function from letter.js
-var Letter = require("./letter.js");
-//Constructor representing current word user is trying to guess.
+// Requiring exported file/function from letter.js
+const Letter = require('./letter.js');
+// Constructor representing current word user is trying to guess.
 function Word(wordArray) {
     this.wordArray = wordArray;
     this.newLet = [];
-    this.hiddenLetter = function() {
-        for (var i = 0; i < wordArray.length; i++) {
-            var alphaLetter = new Letter(wordArray[i]);
+    this.hiddenLetter = () => {
+        for (let i = 0; i < wordArray.length; i += 1) {
+            const alphaLetter = new Letter(wordArray[i]);
             this.newLet.push(alphaLetter);
         }
-    }
-    this.letterForWord = function() {
-        var hiddenWord = [];
-        for (var i = 0; i < this.newLet.length; i++) {
+    };
+    this.letterForWord = () => {
+        const hiddenWord = [];
+        for (let i = 0; i < this.newLet.length; i += 1) {
             hiddenWord.push(this.newLet[i].returning());
         }
-        return hiddenWord.join(" ");
-    }
-    this.checkLet = function(results) {
+        return hiddenWord.join(' ');
+    };
+    this.checkLet = (results) => {
         // console.log("value passed is " + results);
-        
         // console.log("THIs this " + this.newLet[0].guessed);
-        for (var i = 0; i < this.newLet.length; i++) {
+        for (let i = 0; i < this.newLet.length; i += 1) {
             this.newLet[i].check(results);
             // console.log("THIs this " + this.newLet[i].guessed);
         }
-    }
+    };
 }
 
-//Export word.js to other files to require
+// Export word.js to other files to require
 module.exports = Word;
