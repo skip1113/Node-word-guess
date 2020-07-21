93f79a0476f20bd424b94dde95cef207db7e7332
Skip Khamvongsa <skipk95@gmail.com>
2020-07-20 21:28:29 -0600

    Lint letter.js and replaced function

--- letter.js
+++ letter.js
@@ -1,28 +1,26 @@
-//Constructor for letters and guesses to replace a underlying character
+// Constructor for letters and guesses to replace a underlying character
 function Letter(letters) {
     this.letters = letters;
     this.guessed = false;
-    this.returning = function() {
-        if(this.letters === " ") {
-            return " ";
+    this.returning = () => {
+        if (this.letters === ' ') {
+            return ' ';
         }
-        else if(!this.guessed) {
-            return "_";
+        if (!this.guessed) {
+            return '_';
         }
-        else {
-            return this.letters;
-        }
-    }
-    this.check = function(results) {
+
+        return this.letters;
+    };
+    this.check = (results) => {
         // console.log("This.letter is " + results);
         // console.log("This is the .letter " + this.letters);
         // this.guessed = true;
-        if(results.toLowerCase() === this.letters.toLowerCase()) {
-            
+        if (results.toLowerCase() === this.letters.toLowerCase()) {
             this.guessed = true;
             // console.log("correctly Guessed");
         }
-    }
+    };
 }
-//Export this function for other files.
+// Export this function for other files.
 module.exports = Letter;
